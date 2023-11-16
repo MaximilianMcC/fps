010542c0c2b1c78e0198a703f8cca95204422d0e
Max <max@maximilian.co.nz>
2023-11-16 18:58:26 +1300

    Added fullscreen🤢

--- src/Game.cs
+++ src/Game.cs
@@ -85,6 +85,26 @@ class Game
 
 		// Update stuff that can be paused
 		player.Update(deltaTime);
+
+
+
+
+		// TODO: Put somewhere else
+		// Check for if they want to use full screen
+		// TODO: Remove fullscreen and add borderless window
+		if (Raylib.IsKeyPressed(KeyboardKey.KEY_F11))
+		{
+			// Set the width and height of the program to be the monitor size
+			// so when switching to fullscreen the entire pc doesn't die
+			int width = Raylib.GetMonitorWidth(Raylib.GetCurrentMonitor());
+			int height = Raylib.GetMonitorHeight(Raylib.GetCurrentMonitor());
+			Raylib.SetWindowSize(width, height);
+
+			// Toggle fullscreen
+			// TODO: Make it so that the window doesn't minimize when alt+tabbing
+			Raylib.ToggleFullscreen();
+		}
+
 	}
 
 	// TODO: Render in 4:3 squished
