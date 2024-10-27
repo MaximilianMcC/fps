33657fe8fc4a11e4928d47ac94d730a3950dc60b
MaximilianMcC <max@maximilian.co.nz>
2024-10-28 09:53:03 +1300

    Player can look around

--- src/Entity/Player.cs
+++ src/Entity/Player.cs
@@ -4,7 +4,12 @@ using Raylib_cs;
 class Player : Entity
 {
 	public Camera3D Camera;
-	private float EyeHeight = 1.7f;
+	Quaternion rotation = Quaternion.Identity;
+	public float Yaw;
+	public float Pitch;
+
+	private float eyeHeight = 1.7f;
+	private float sensitivity = 250f;
 
 	public override void Start()
 	{
@@ -27,13 +32,36 @@ class Player : Entity
 		// Target.Y = Vertical rotation
 		// Target.Z = Direction
 
-		Camera.Position = Position + (Vector3.UnitY * EyeHeight);
-		Camera.Target = Position + (Vector3.UnitY * EyeHeight) + Vector3.UnitZ;
+		// Get the direction (Z) using the
+		// rotation quaternion
+		Vector3 direction = Vector3.Transform(Vector3.UnitZ, rotation);
+
+		// Update the position, and the
+		// target based on the position
+		Camera.Position = Position + (Vector3.UnitY * eyeHeight);
+		Camera.Target = Position + (Vector3.UnitY * eyeHeight) + direction;
+	}
+
+	private void Movement()
+	{
+		// Get what direction the player is looking at
+		// and update the yaw and pitch accordingly
+		//? Using 89 instead of 90 to avoid gimbal lock
+		Vector2 mouseMovement = Raylib.GetMouseDelta() * sensitivity * Raylib.GetFrameTime();
+		Yaw -= mouseMovement.X;
+		Pitch = Math.Clamp(Pitch + mouseMovement.Y, -89f, 89f);
+
+		// Update the quaternion rotation thingy based
+		// on the newly updated yaw and pitch values
+		// converted to radians 
+		float yaw = Yaw * Raylib.DEG2RAD;
+		float pitch = Pitch * Raylib.DEG2RAD;
+		rotation = Quaternion.CreateFromYawPitchRoll(yaw, pitch, 0f);
 	}
 
 	public override void Update()
 	{
-		Position.X += 10 * Raylib.GetFrameTime();
+		Movement();
 		UpdateCamera();
 	}
 
