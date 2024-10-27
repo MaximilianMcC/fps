965132e59d273020f8aa23967a8f35e158041f13
MaximilianMcC <max@maximilian.co.nz>
2024-10-28 10:21:56 +1300

    freecam type movement

--- src/Entity/Player.cs
+++ src/Entity/Player.cs
@@ -4,9 +4,14 @@ using Raylib_cs;
 class Player : Entity
 {
 	public Camera3D Camera;
-	Quaternion rotation = Quaternion.Identity;
+
+	// Head stuff
 	public float Yaw;
 	public float Pitch;
+	Quaternion rotation = Quaternion.Identity;
+
+	// Actual moving stuff
+	float speed = 45f;
 
 	private float eyeHeight = 1.7f;
 	private float sensitivity = 250f;
@@ -33,7 +38,7 @@ class Player : Entity
 		// Target.Z = Direction
 
 		// Get the direction (Z) using the
-		// rotation quaternion
+		// rotation quaternion thingy
 		Vector3 direction = Vector3.Transform(Vector3.UnitZ, rotation);
 
 		// Update the position, and the
@@ -44,19 +49,48 @@ class Player : Entity
 
 	private void Movement()
 	{
-		// Get what direction the player is looking at
-		// and update the yaw and pitch accordingly
-		//? Using 89 instead of 90 to avoid gimbal lock
-		Vector2 mouseMovement = Raylib.GetMouseDelta() * sensitivity * Raylib.GetFrameTime();
-		Yaw -= mouseMovement.X;
-		Pitch = Math.Clamp(Pitch + mouseMovement.Y, -89f, 89f);
+		// Mouse movement/looking around
+		{
+			// Get what direction the player is looking at
+			// and update the yaw and pitch accordingly
+			//? Using 89 instead of 90 to avoid gimbal lock
+			Vector2 mouseMovement = Raylib.GetMouseDelta() * sensitivity * Raylib.GetFrameTime();
+			Yaw -= mouseMovement.X;
+			Pitch = Math.Clamp(Pitch + mouseMovement.Y, -89f, 89f);
 
-		// Update the quaternion rotation thingy based
-		// on the newly updated yaw and pitch values
-		// converted to radians 
-		float yaw = Yaw * Raylib.DEG2RAD;
-		float pitch = Pitch * Raylib.DEG2RAD;
-		rotation = Quaternion.CreateFromYawPitchRoll(yaw, pitch, 0f);
+			// Update the quaternion rotation thingy based
+			// on the newly updated yaw and pitch values
+			// converted to radians 
+			float yaw = Yaw * Raylib.DEG2RAD;
+			float pitch = Pitch * Raylib.DEG2RAD;
+			rotation = Quaternion.CreateFromYawPitchRoll(yaw, pitch, 0f);
+		}
+
+		// Keyboard movement/moving around
+		{
+			// Capture all the left/right movement
+			int xMovement = 0;
+			if (Raylib.IsKeyDown(KeyboardKey.A)) xMovement += 1;
+			if (Raylib.IsKeyDown(KeyboardKey.D)) xMovement += -1;
+
+			// Capture all the forwards/backwards movement
+			int yMovement = 0;
+			if (Raylib.IsKeyDown(KeyboardKey.W)) yMovement += 1;
+			if (Raylib.IsKeyDown(KeyboardKey.S)) yMovement += -1;
+
+			// Apply the direction vector to the movement
+			// TODO: Don't reuse this from UpdateCamera
+			// TODO: Just plus this on in UpdateCamera or something
+			Vector3 directionInput = new Vector3(xMovement, 0, yMovement);
+			Vector3 direction = Vector3.Transform(directionInput, rotation);
+
+			// Apply speed and whatnot to get the movement
+			// TODO: Velocity based movement system
+			Vector3 movement = (direction * speed) * Raylib.GetFrameTime();
+
+			// Update the players position with the new movement
+			Position += movement;
+		}
 	}
 
 	public override void Update()
